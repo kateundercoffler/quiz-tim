da3cae762f6e533d193a661aa43cb6835be1eae4
Kathryn Undercoffler <kathryn.grce@gmail.com>
2023-04-28 13:54:44 -0400

    Changements du texte pour identifier avec le sujet

--- kateundercoffler/assets/js/app.js
+++ kateundercoffler/assets/js/app.js
@@ -1,6 +1,6 @@
 /**
  * TIM - QUIZ
- * @author: Prénom Nom;
+ * @author: Kate Undercoffler;
  **/
 
 /* Données du quiz - À ADAPTER SELON LES CONTENUS DU QUIZ */
@@ -8,25 +8,25 @@
 des input[type=radio] */
 const objJSON = {
     "retroactions": {
-        "positive": "Bravo, c’est une bonne réponse !",
-        "negative": "Désolé, ce n’est pas la bonne réponse"
+        "positive": "Bravo, c'est une bonne réponse !",
+        "negative": "Bon essai, mais la réponse attendue est !"
     },
     "explications": {
-        "Q1": "Explications de la question 1",
-        "Q2": "Explications de la question 2",
-        "Q3": "Explications de la question 3"
+        "Q1": "Le CSR peut contrôler la surpopulation de chats, diminuer les nuisances causées par les comportements de chats fertiles et diminuer les chats qui sont euthanasiés.",
+        "Q2": "Un coup d'oreille ou « ear tip » en anglais est un moyen que les gens faisant le CSR peuvent utiliser pour savoir si un chat a déjà participé dans le CSR et sinon, il peut être capturé humainement.",
+        "Q3": "C'est important que le chat soit ramené dans un lieu familier car il connaît son environnement et il s'y est habitué. Sa communauté est comme sa maison !"
     },
     "bonnesReponses": [
-        "Q1A",
+        "Q1B",
         "Q2C",
         "Q3B"
     ],
     "messages": {
         "resultatsDebut": "Vous avez obtenu un résultat de",
-        "note0": "Vous auriez pu faire mieux. Je vous suggère ..",
-        "note33": "Vous auriez pu faire mieux. Je vous suggère ..",
-        "note66": "Bravo, vous avez une bonne connaissance générale de ...",
-        "note100": "Félicitations, vous êtes un fin connaisseur !"
+        "note0": "Awh, bon essai. Vous auriez pu faire mieux, il y a beaucoup d'information sur le CSR en ligne, faites la recherche et essayez encore plus tard",
+        "note33": "Pas trop pire, vous auriez pu faire mieux. Je vous suggère de googler le CSR.",
+        "note66": "Wow, tellement bon, vous connaissez le CSR en général!",
+        "note100": "Félicitations, vous êtes un CSR héro ! Avez-vous déjà aidé les chats à Québec?"
     }
 };
 
